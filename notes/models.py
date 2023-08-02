3cd9907a3a110d52cfcfb8b10ff73fea58576e3e
Ujjawal Shrivastava <ujjawalshrivastava19@gmail.com>
2023-08-02 06:49:19 +0530

    Added: model, serializer & urls

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField()
+    pinned = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="note_images/", blank=True, null=True)
+    background_color = models.CharField(max_length=20, blank=True, null=True)
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
